1b1b5cd6d4671286f955fde380892797e0a9321b
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2021-04-28 19:11:09 +0900

    Update Main.php

--- src/MultiProtocol/Main.php
+++ src/MultiProtocol/Main.php
@@ -11,30 +11,15 @@ use pocketmine\network\mcpe\protocol\LoginPacket;
 
 
 class Main extends PluginBase implements Listener {
-	
-   public $acceptProtocol = [];
 
-   public function onEnable() {
+    public function onEnable() {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->getLogger()->info("VRZ-MultiProtocol Enable!");
-		
-		@mkdir($this->getDataFolder());
-		$this->acceptProtocol = (new Config($this->getDataFolder()."accept.yml", Config::YAML))->get("accept-protocol");
-		
-		if ($this->acceptProtocol === false || empty($this->acceptProtocol)) {
-			$this->acceptProtocol[] = ProtocolInfo::CURRENT_PROTOCOL;
-			$config = new Config($this->getDataFolder()."accept.yml", Config::YAML);
-			$config->set("accept-protocol", [ProtocolInfo::CURRENT_PROTOCOL]);
-			$config->save();
-		}
     }
     
     public function onDataPacketRecieve (DataPacketReceiveEvent $ev) {
     	$pk = $ev->getPacket();
     	if ($pk instanceof LoginPacket) {
-    		if (in_array($pk->protocol, $this->acceptProtocol)) {
-    			$pk->protocol = ProtocolInfo::CURRENT_PROTOCOL;
-    		}
+    		$pk->protocol = ProtocolInfo::CURRENT_PROTOCOL;
     	}
     }
 }
